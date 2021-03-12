3c5ab9e50dc1af75d74d74e0ec845ca88e085cc6
diogomatoschaves <diogo_chaves@hotmail.com>
2021-03-12 12:13:57 +0000

    Rearrange folder structure

--- data_processing/extract/scrapers/lunarcrush/session_key_handler.py
+++ data_processing/extract/scrapers/lunarcrush/session_key_handler.py
@@ -2,7 +2,7 @@ import json
 
 import requests
 
-from data_processing.scrapers.lunarcrush.headers import google_api_headers, get_headers
+from data_processing.extract.scrapers.lunarcrush.headers import google_api_headers, get_headers
 
 
 def get_google_auth():
