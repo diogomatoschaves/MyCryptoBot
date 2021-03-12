98319727c71bd34c57d985e6d046f64f8605aafa
diogomatoschaves <diogo_chaves@hotmail.com>
2021-03-12 11:47:12 +0000

    Move twitter scraper

--- scrapers/twitter/twitter.py
+++ scrapers/twitter/twitter.py
@@ -1,61 +1,167 @@
-import requests
-from requests.exceptions import ConnectionError
+import os
+from itertools import combinations
+from datetime import timedelta, datetime
 
-from scrapers.helpers.headers import get_headers
+import django
+from dateutil.parser import parse
+from scrapers.twitter.tweets_fetcher import get_tweets
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "crypto_db.settings")
+django.setup()
 
-def get_tweets(words, hashtags, from_date, until_date, min_favs=None):
-
-    headers = get_headers()
-
-    words_string = '(' + '%20OR%20'.join(words) + ')%20'
-
-    hashtags_string = '(' + '%20OR%20'.join(['%23' + hashtag for hashtag in hashtags]) + ')%20'
-
-    if not min_favs:
-        min_favs_string = ''
-    else:
-        min_favs_string = f'min_faves%3A{min_favs}%20'
-
-    query_string = f"include_profile_interstitial_type=1&include_blocking=1&include_blocked_by=1&" \
-                   "include_followed_by=1&include_want_retweets=1&include_mute_edge=1&" \
-                   "include_can_dm=1&include_can_media_tag=1&skip_status=1&cards_platform=Web-12&" \
-                   "include_cards=1&include_ext_alt_text=true&include_quote_count=true&" \
-                   "include_reply_count=1&tweet_mode=extended&include_entities=true&" \
-                   "include_user_entities=true&include_ext_media_color=true&include_ext_media_availability=true&" \
-                   f"send_error_codes=true&simple_quoted_tweet=true&" \
-                   f"q={words_string}{hashtags_string}{min_favs_string}until%3A{until_date}%20since%3A{from_date}" \
-                   f"&count=1000&query_source=typed_query&pc=1&spelling_corrections=1&" \
-                   "ext=mediaStats%2ChighlightedLabel"
-
-    host = "https://twitter.com/i/api/2/search/adaptive.json?"
-
-    url = host + query_string
-
-    retries = 0
-    while True:
-        try:
-            r = requests.get(url, headers=headers)
-            break
-        except ConnectionError:
-            retries += 1
-            if retries > 2:
-                raise Exception(f'Too many retries. {from_date}')
-
-    return r.json()
+from database.model.models import Tweet, TwitterUser, Hashtag, LunarCrushTimeEntries
 
 
-if __name__ == "__main__":
+def daterange(start_date, end_date):
 
-    from_date = '2014-03-01'
-    until_date = '2014-03-02'
+    start_date = datetime.strptime(start_date, "%Y-%m-%d").date()
+    end_date = datetime.strptime(end_date, "%Y-%m-%d").date()
 
-    words = ['bitcoin', 'btc']
-
-    hashtags = ['cryptocurrency', 'btc', 'bitcoin']
-
-    response = get_tweets(words, hashtags, from_date, until_date)
+    for n in range(int((end_date - start_date).days)):
+        yield start_date + timedelta(n)
 
 
+def process_users(users):
+
+    user_ids = set()
+
+    for user_id, user in users.items():
+
+        user_ids.add(user_id)
+
+        created_at = parse(user["created_at"])
+
+        user_obj = TwitterUser(
+            id = user_id,
+            name = user["name"],
+            location = user["location"],
+            description = user["description"],
+            followers_count = user["followers_count"],
+            friends_count = user["friends_count"],
+            listed_count = user["listed_count"],
+            created_at = created_at,
+            favourites_count = user["favourites_count"],
+            statuses_count = user['statuses_count'],
+            media_count = user["media_count"],
+            blocked_by = user["blocked_by"],
+            blocking = user["blocking"]
+        )
+
+        user_obj.save()
+
+    return user_ids
 
 
+def process_tweets(tweets):
+
+    tweet_ids = set()
+
+    for tweet_id, tweet in tweets.items():
+
+        tweet_ids.add(tweet_id)
+
+        created_at = parse(tweet["created_at"])
+        user = TwitterUser.objects.get(id=tweet["user_id_str"])
+
+        tweet_obj = LunarCrushTimeEntries(
+            id = tweet_id,
+            created_at = created_at,
+            full_text = tweet["full_text"],
+            number_hashtags = len(tweet["entities"]["hashtags"]),
+            number_symbols = len(tweet["entities"]["symbols"]),
+            user_mentions = len(tweet["entities"]["user_mentions"]),
+            number_urls = len(tweet["entities"]["urls"]),
+            number_media = len(tweet["entities"]["media"]) if "media" in tweet["entities"] else 0,
+            user = user,
+            in_reply_to_status = True if tweet['in_reply_to_status_id'] else False,
+            has_quoted_status = True if tweet['is_quote_status'] in tweet else False,
+            contributors = tweet["contributors"],
+            retweet_count = tweet["retweet_count"],
+            favourite_count = tweet["favorite_count"],
+            reply_count = tweet["reply_count"],
+            quote_count = tweet["quote_count"],
+            possibly_sensitive = tweet["possibly_sensitive"] if "possibly_sensitive" in tweet else False
+        )
+
+        tweet_obj.save()
+
+        for hashtag in tweet["entities"]["hashtags"]:
+            hashtag_obj = Hashtag.objects.get_or_create(name=hashtag["text"].lower())[0]
+
+            tweet_obj.hashtags.add(hashtag_obj)
+
+    return tweet_ids
+
+
+def process_response(user_ids, tweet_ids, response):
+
+    new_user_ids = process_users(response["globalObjects"]["users"])
+
+    new_tweet_ids = process_tweets(response["globalObjects"]["tweets"])
+
+    user_ids = user_ids.union(new_user_ids)
+    tweet_ids = tweet_ids.union(new_tweet_ids)
+
+    return user_ids, tweet_ids
+
+
+def combine_search(words, hashtags, from_date, until_date, user_ids, tweet_ids):
+
+    comb_words = ['a']
+    comb_hashtags = ['a']
+    i = 1
+
+    while len(comb_words) != 0 or len(comb_hashtags) != 0:
+        comb_words = list(combinations(words, i))
+        comb_hashtags = list(combinations(hashtags, i))
+
+        for word_comb in comb_words:
+            response = get_tweets(word_comb, [], from_date, until_date)
+
+            try:
+                user_ids, tweet_ids = process_response(user_ids, tweet_ids, response)
+            except KeyError:
+                response = get_tweets(word_comb, [], from_date, until_date)
+
+        for hashtag_comb in comb_hashtags:
+            response = get_tweets([], hashtag_comb, from_date, until_date)
+
+            try:
+                user_ids, tweet_ids = process_response(user_ids, tweet_ids, response)
+            except KeyError:
+                response = get_tweets([], hashtag_comb, from_date, until_date)
+
+        i += 1
+
+    return user_ids, tweet_ids
+
+
+def fetch_twitter_data(start_date, end_date, words, hashtags):
+
+    for single_date in daterange(start_date, end_date):
+
+        from_date = single_date.strftime("%Y-%m-%d")
+        until_date = (single_date + timedelta(days=1)).strftime("%Y-%m-%d")
+
+        print(from_date)
+
+        user_ids = set()
+        tweet_ids = set()
+
+        user_ids, tweet_ids = combine_search(words, [], from_date, until_date, user_ids, tweet_ids)
+        user_ids, tweet_ids = combine_search([], hashtags, from_date, until_date, user_ids, tweet_ids)
+
+        print(f'New Tweets: {len(tweet_ids)}')
+        print(f'New Users: {len(user_ids)}')
+        print()
+
+
+if __name__ == '__main__':
+
+    start_date = '2020-02-17'
+    end_date = '2021-02-21'
+
+    words = ["bitcoin", "bitcoins", "btc"]
+    hashtags = ["cryptocurrency", "bitcoin", "btc"]
+
+    fetch_twitter_data(start_date, end_date, words, hashtags)
