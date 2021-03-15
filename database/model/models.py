236de12e5bc996475650d5f57e126e37de287660
diogomatoschaves <diogo_chaves@hotmail.com>
2021-03-15 13:54:09 +0000

    Reorganize structure

--- database/model/models.py
+++ database/model/models.py
@@ -11,4 +11,40 @@ class Asset(models.Model):
 
     symbol = models.TextField(primary_key=True)
     name = models.TextField(null=True)
+    slug = models.TextField(null=True)
+    stats_date = models.DateTimeField(null=True)
+    market_cap_ranking = models.IntegerField(null=True)
+
+    category = models.TextField(null=True)
+    sector = models.TextField(null=True)
+    overview = models.TextField(null=True)
+    technology = models.TextField(null=True)
+    tagline = models.TextField(null=True)
+
+    all_time_high_price = models.FloatField(null=True)
+    all_time_high_date = models.DateTimeField(null=True)
+    developer_activity_stars = models.IntegerField(null=True)
+    developer_activity_commits_last_3_months = models.IntegerField(null=True)
+    developer_activity_commits_last_1_year = models.IntegerField(null=True)
+    asset_created_at = models.DateTimeField(null=True)
+
+    token_usage = models.TextField(null=True)
+    token_type = models.TextField(null=True)
+    token_launch_style = models.TextField(null=True)
+    token_initial_supply = models.TextField(null=True)
+    token_is_treasury_decentralized = models.NullBooleanField(null=True)
+    token_mining_algorithm = models.TextField(null=True)
+    token_next_halving_date = models.TextField(null=True)
+    token_emission_type_general = models.TextField(null=True)
+    token_emission_type_precise = models.TextField(null=True)
+    token_max_supply = models.FloatField(null=True)
+
+    relevant_resources = models.ManyToManyField('AssetResources')
+
+
+class AssetResources(models.Model):
+
+    name = models.TextField(null=True)
+    url = models.TextField(null=True)
+
 
