311ef5e90764ff1b6b3a0a3ca49d4113f4b760ab
diogomatoschaves <diogo_chaves@hotmail.com>
2021-03-18 17:50:39 +0000

    Factor out strategies from Vectorized classes

--- strategies/backtesting/vectorized/momentum.py
+++ strategies/backtesting/vectorized/momentum.py
@@ -1,54 +1,20 @@
 import numpy as np
 
+from strategies.backtesting.strategies import MomentumBase
 from strategies.backtesting.vectorized.base import VectorizedBacktester
 
 
-class MomentumVectBacktester(VectorizedBacktester):
+class MomentumVectBacktester(MomentumBase, VectorizedBacktester):
     """ Class for the vectorized backtesting of simple Contrarian trading strategies.
-
-    Attributes
-    ==========
-    symbol: str
-        ticker symbol with which to work with
-    start: str
-        start date for data retrieval
-    end: str
-        end date for data retrieval
-    tc: float
-        proportional transaction costs per trade
-
-    Methods
-    =======
-    get_data:
-        retrieves and prepares the data
-
-    test_strategy:
-        runs the backtest for the contrarian-based strategy
-
-    plot_results:
-        plots the performance of the strategy compared to buy and hold
     """
 
     def __init__(self, data, window=10, trading_costs=0, symbol='BTCUSDT'):
-        super().__init__(data, symbol=symbol, trading_costs=trading_costs)
+        MomentumBase.__init__(self)
+        VectorizedBacktester.__init__(self, data, symbol=symbol, trading_costs=trading_costs)
 
         self.window = window
 
         self._update_data()
-
-    def __repr__(self):
-        return "{}(symbol = {}, window = {})".format(self.__class__.__name__, self.symbol, self.window)
-
-    def _update_data(self):
-        """ Retrieves and prepares the data.
-        """
-        super()._update_data()
-
-    def _set_parameters(self, window):
-        """ Updates SMA parameters and resp. time series.
-        """
-        if window is not None:
-            self.window = int(window)
 
     def test_strategy(self, window=None, plot_results=True):
         """ Backtests the trading strategy.
@@ -56,7 +22,7 @@ class MomentumVectBacktester(VectorizedBacktester):
         self._set_parameters(window)
 
         data = self.data.copy().dropna()
-        data["position"] = np.sign(data["returns"].rolling(self.window).mean())
+        data["position"] = np.sign(data[self.returns_col].rolling(self.window).mean())
 
         title = self.__repr__()
 
