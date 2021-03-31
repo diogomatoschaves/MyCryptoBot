d678258e6ff4ffcf14a9d4c7f263152af679a879
Diogo Matos Chaves <diogo_chaves@hotmail.com>
2021-03-31 13:54:50 +0100

    Add new strategy: SMA

--- quant_model/strategies/__init__.py
+++ quant_model/strategies/__init__.py
@@ -1,4 +1,5 @@
 from quant_model.strategies.momentum import MomentumBase
 from quant_model.strategies.mean_reversion import MeanRevBase
+from quant_model.strategies.sma_crossover import SMACrossoverBase
 from quant_model.strategies.sma import SMABase
 from quant_model.strategies.predictive import MLBase
--- quant_model/strategies/sma_crossover.py
+++ quant_model/strategies/sma_crossover.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-class SMABase:
+class SMACrossoverBase:
     """ Class for the vectorized backtesting of SMA-based trading strategies.
     """
 
