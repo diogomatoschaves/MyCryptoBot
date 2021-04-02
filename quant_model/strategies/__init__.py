8a33bd2ab7f0470af3392c10613f49cd4124779c
Diogo Matos Chaves <diogo_chaves@hotmail.com>
2021-04-02 17:29:33 +0100

    Pass estimator object directly to ML strategy class

--- quant_model/strategies/__init__.py
+++ quant_model/strategies/__init__.py
@@ -2,3 +2,4 @@ from quant_model.strategies.moving_average.macd import MACD
 from quant_model.strategies.trend.momentum import Momentum
 from quant_model.strategies.moving_average.ma_crossover import MACrossover
 from quant_model.strategies.moving_average.ma import MA
+from quant_model.strategies.mean_reversion.bollinger_bands import BollingerBands
