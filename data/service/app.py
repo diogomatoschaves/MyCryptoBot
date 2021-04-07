3f71fb318c0d210b4fdee6c3dec1597341a9aaff
Diogo Matos Chaves <diogo_chaves@hotmail.com>
2021-04-07 22:01:18 +0100

    Add first data service stone; Add dockerfile.

---
+++ data/service/app.py
@@ -0,0 +1,37 @@
+from flask import Flask, jsonify, request
+
+# from data.extrac
+from werkzeug.exceptions import BadRequest
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+@app.route('/start_bot', methods=['PUT'])
+def start_bot():
+
+    try:
+        data = request.get_json(force=True)
+    except BadRequest:
+        pass
+
+
+
+    return jsonify({"response": "success"})
+
+
+@app.route('/stop_bot')
+def stop_bot():
+
+    # Stops the bot
+    # closes any open positions
+
+    pass
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
