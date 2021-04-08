9e57d0629ee49d5e8291c7529ba570197de259ef
Diogo Matos Chaves <diogo_chaves@hotmail.com>
2021-04-08 17:24:35 +0100

    Update entrypoint file

--- data/entrypoint.sh
+++ data/entrypoint.sh
@@ -1,18 +1,5 @@
 #!/bin/sh
 
-if [ "$DATABASE" = "postgres" ]
-then
-    echo "Waiting for postgres..."
-
-    while ! nc -z "$SQL_HOST" "$SQL_PORT"; do
-      sleep 0.1
-    done
-
-    echo "PostgreSQL started"
-fi
-
-python database/manage.py migrate
-
 cd data/service/
 
 exec gunicorn --bind 0.0.0.0:"$PORT" wsgi:app
