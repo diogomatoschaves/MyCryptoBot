c1463b3f32c1f2acaa753b4cf1b23a6b86d98416
Diogo Matos Chaves <diogo_chaves@hotmail.com>
2021-04-08 16:40:24 +0100

    Update entrypoint.sh and settings.py

--- database/settings.py
+++ database/settings.py
@@ -1,6 +1,9 @@
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import dj_database_url
+
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 DATABASES = {
@@ -10,6 +13,10 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# Heroku: Update database configuration from $DATABASE_URL.
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 INSTALLED_APPS = (
     'database.model',
