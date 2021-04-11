ed5b737a7b5d3437739ac554737b812075d0eb22
Diogo Matos Chaves <diogo_chaves@hotmail.com>
2021-04-11 22:18:07 +0100

    Handle ConnectionErrors gracefuly

--- shared/exchanges/binance/_binance.py
+++ shared/exchanges/binance/_binance.py
@@ -2,6 +2,7 @@ from os import environ as env
 
 from dotenv import load_dotenv, find_dotenv
 from binance.client import Client
+from requests import ReadTimeout, ConnectionError
 
 import shared.exchanges.binance.constants as const
 
@@ -12,12 +13,23 @@ if ENV_FILE:
 
 class BinanceHandler(Client):
 
-    def __init__(self):
+    def __init__(self, base_candle_size='5m'):
         # self.API_KEY = 'https://testnet.binance.vision/api'
+
+        self.base_candle_size = base_candle_size
 
         self._get_api_keys()
 
-        Client.__init__(self, self.binance_api_key, self.binance_api_secret)
+        retries = 0
+
+        while True:
+            try:
+                Client.__init__(self, self.binance_api_key, self.binance_api_secret)
+                break
+            except (ConnectionError, ReadTimeout) as e:
+                retries += 1
+                if retries >= 3:
+                    raise ConnectionError(e)
 
     def _get_api_keys(self):
         self.binance_api_key = env.get(const.BINANCE_API_KEY)
