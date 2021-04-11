f3cf7522439c78356ff2c92d73c49224d0bb5801
Diogo Matos Chaves <diogo_chaves@hotmail.com>
2021-04-11 22:23:02 +0100

    Add functionality to start data pipelines in the background

--- data/etl_pipeline.py
+++ data/etl_pipeline.py
@@ -1,8 +1,17 @@
+import logging
+import time
+
 from data.binance import BinanceDataHandler
+from shared.utils.logger import configure_logger
+
+configure_logger()
 
 
-def start_data_collection(base, quote, candle_size='1h'):
+def start_data_collection(base, quote, candle_size):
 
     binance_handler = BinanceDataHandler(base, quote, candle_size)
 
     binance_handler.start_data_ingestion()
+
+    while True:
+        time.sleep(10)
