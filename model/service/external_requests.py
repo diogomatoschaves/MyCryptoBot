d7bce19e6b681b23b4534142d3927f88b4c13222
Diogo Matos Chaves <diogo_chaves@hotmail.com>
2021-04-16 12:01:20 +0100

    Refactor model service; Connect to execution service

---
+++ model/service/external_requests.py
@@ -0,0 +1,30 @@
+import logging
+import os
+
+import requests
+
+from model.service.helpers import EXECUTION_APP_ENDPOINTS
+from shared.utils.decorators.failed_connection import retry_failed_connection
+
+
+@retry_failed_connection(num_times=3)
+def execute_order(symbol, signal, exchange):
+
+    url = EXECUTION_APP_ENDPOINTS["EXECUTE_ORDER"](os.getenv("EXECUTION_APP_URL"), exchange)
+
+    payload = {
+        "symbol": symbol,
+        "signal": signal
+    }
+
+    position = "GO LONG" if signal == 1 else "GO SHORT" if signal == -1 else "GO NEUTRAL"
+
+    logging.info(f"{symbol}: Sending {position} order to {exchange}.")
+
+    r = requests.post(url, json=payload)
+    logging.debug(r.text)
+
+    response = r.json()
+    logging.debug(response["response"])
+
+    return response
