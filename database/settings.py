90f159a04cda66a6aab2268f11bd72555ab1469b
Diogo Matos Chaves <diogo_chaves@hotmail.com>
2021-04-16 16:29:07 +0100

    Refactor tests to use mock db

--- database/settings.py
+++ database/settings.py
@@ -6,17 +6,25 @@ import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'crypto_bot',
-        'USER': 'postgres',
-        'PASSWORD': '6730',
-        'HOST': 'localhost',
-        'PORT': '5432',
-        'CONN_MAX_AGE': 500
+if os.getenv("TEST", False):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'crypto_bot',
+            'USER': 'postgres',
+            'PASSWORD': '6730',
+            'HOST': 'localhost',
+            'PORT': '5432',
+            'CONN_MAX_AGE': 500
+        }
+    }
 
 # Heroku: Update database configuration from $DATABASE_URL.
 db_from_env = dj_database_url.config(conn_max_age=500)
