06d9f73c51eb8b2202ffa74df6f77b14b585cad9
Diogo Matos Chaves <diogo_chaves@hotmail.com>
2021-04-19 21:23:07 +0100

    Add tests for binance load module

--- data/tests/helpers/mocks/models.py
+++ data/tests/helpers/mocks/models.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import pytz
 import pytest
 
+from data.tests.helpers.sample_data import exchange_data_1
 from database.model.models import Exchange, Symbol, ExchangeData
 
 
@@ -19,25 +20,9 @@ def create_symbol(db):
 @pytest.fixture
 def exchange_data_factory(db, create_exchange, create_symbol):
     # Closure
-    def create_exchange_data(
-        open_time=datetime(2019, 9, 2, 10, tzinfo=pytz.utc),
-        close_time=datetime(2019, 9, 1, 11, tzinfo=pytz.utc)
-    ):
+    def create_exchange_data(**kwargs):
         return ExchangeData.objects.create(
-            exchange=create_exchange,
-            symbol=create_symbol,
-            open_time=open_time,
-            close_time=close_time,
-            interval='1h',
-            open=1000,
-            high=1010,
-            low=990,
-            close=1005,
-            volume=10,
-            quote_volume=1E4,
-            trades=2,
-            taker_buy_asset_volume=5,
-            taker_buy_quote_volume=5E3,
+            **dict(**exchange_data_1, **kwargs)
         )
     return create_exchange_data
 
