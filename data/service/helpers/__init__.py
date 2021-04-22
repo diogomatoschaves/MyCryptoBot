be682e9dc52a64327ca453ec76a43ae462cf89df
Diogo Matos Chaves <diogo_chaves@hotmail.com>
2021-04-22 17:17:29 +0100

    Add tests for data service

--- data/service/helpers/__init__.py
+++ data/service/helpers/__init__.py
@@ -1,1 +1,1 @@
-from data.service.helpers._helpers import check_input, STRATEGIES, APP_NAME, MODEL_APP_ENDPOINTS, EXECUTION_APP_ENDPOINTS
+from data.service.helpers._helpers import check_input, STRATEGIES, MODEL_APP_ENDPOINTS, EXECUTION_APP_ENDPOINTS
