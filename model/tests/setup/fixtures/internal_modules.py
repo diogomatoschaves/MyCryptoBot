b72d3e42ad702ad0957f05c18604fd2a9811842b
Diogo Matos Chaves <diogo_chaves@hotmail.com>
2021-04-23 23:07:16 +0100

    Add tests for external requests and strategies get_signal

--- model/tests/setup/fixtures/internal_modules.py
+++ model/tests/setup/fixtures/internal_modules.py
@@ -2,12 +2,64 @@ import os
 
 import pytest
 
+import model
+
 
 TEST_APP_NAME = 'test_app'
+EXECUTION_APP_URL = 'https://example.com'
 
 
 @pytest.fixture()
 def mock_settings_env_vars(mocker):
     mocker.patch.dict(os.environ, {
         "APP_NAME": TEST_APP_NAME,
+        "EXECUTION_APP_URL": EXECUTION_APP_URL,
     })
+
+
+@pytest.fixture()
+def mock_get_data(mocker):
+    return mocker.patch('model.strategies._strategies.get_data')
+
+
+@pytest.fixture()
+def mock_trigger_order(mocker):
+    return mocker.patch('model.strategies._strategies.trigger_order')
+
+
+@pytest.fixture()
+def mock_execute_order(mocker):
+    return mocker.patch('model.strategies._strategies.execute_order')
+
+
+def mock_strategy(*args, **kwargs):
+    class MockStrategy:
+        def __init__(self, *args1, **kwargs1):
+            pass
+
+        def get_signal(self):
+            return 1
+
+    return MockStrategy()
+
+
+def mock_strategy_factory(strategy):
+    def mock_strategy(*args, **kwargs):
+        class MockStrategy:
+            def __init__(self, *args1, **kwargs1):
+                pass
+
+            def get_signal(self):
+                return 1
+
+        return MockStrategy()
+
+    @pytest.fixture()
+    def mocked_strategy(mocker):
+        mocker.patch.object(
+            model.strategies._strategies,
+            strategy,
+            mock_strategy
+        )
+
+    return mocked_strategy
