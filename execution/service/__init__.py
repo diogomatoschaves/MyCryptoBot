a9e33eb025b3e25b9eb4a6d4fabc7657523333be
Diogo Matos Chaves <diogo_chaves@hotmail.com>
2021-04-27 21:59:40 +0100

    Add tests for execution service

---
+++ execution/service/__init__.py
@@ -0,0 +1,15 @@
+import os
+
+import execution.service.app as application
+
+
+def create_app(testing=False, env_vars=None):
+    """Create and configure an instance of the Flask application."""
+
+    if env_vars is not None:
+        for key, value in env_vars.items():
+            os.environ.setdefault(key, value)
+
+    application.app.config['TESTING'] = testing
+
+    return application.app
