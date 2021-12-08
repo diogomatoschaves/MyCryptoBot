8236cde39a1146b2ceb0223cfde422c7e78b2733
Diogo Matos Chaves <diogo_chaves@hotmail.com>
2021-12-08 18:52:21 +0100

    Add support for paper trading

--- execution/exchanges/binance/mock/_trading.py
+++ execution/exchanges/binance/mock/_trading.py
@@ -1,8 +1,15 @@
+import os
+import time
 from random import randint
 
-import shared.exchanges.binance.constants as const
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "database.settings")
+django.setup()
+
+from database.model.models import StructuredData
 from execution.exchanges.binance._trading import BinanceTrader
-from execution.tests.setup.test_data.binance_api_responses import order_creation, trading_fees, isolated_account_info
+from execution.tests.setup.test_data.binance_api_responses import trading_fees, isolated_account_info
 
 
 class BinanceMockTrader(BinanceTrader):
@@ -16,8 +23,6 @@ class BinanceMockTrader(BinanceTrader):
             margin_level=margin_level,
             paper_trading=True
         )
-
-        # self.create_test_order()
 
     def _init_session(self):
         pass
@@ -41,15 +46,45 @@ class BinanceMockTrader(BinanceTrader):
         return {"tranId": 100000001}
 
     def create_margin_order(
-            self,
-            symbol,
-            side,
-            type,
-            newOrderRespType,
-            isIsolated,
-            sideEffectType,
-            quantity=None,
-            quoteOrderQty=None,
+        self,
+        symbol,
+        side,
+        type,
+        newOrderRespType,
+        isIsolated,
+        sideEffectType,
+        quantity=None,
+        quoteOrderQty=None,
+        **kwargs
     ):
-        if newOrderRespType == "FULL":
-            return {**order_creation, "orderId": randint(1, 1E9)}
+        transact_time = int(time.time() * 1000)
+
+        price = StructuredData.objects.last().close
+
+        quantity = quantity if quantity else quoteOrderQty
+
+        print(price, quantity)
+
+        return {
+            "symbol": symbol,
+            "orderId": randint(1, 1E9),
+            "clientOrderId": "6gCrw2kRUAF9CvJDGP16IP",
+            "transactTime": transact_time,
+            "price": str(price),
+            "origQty": str(quantity),
+            "executedQty": str(quantity),
+            "cummulativeQuoteQty": str(quantity),
+            "status": "FILLED",
+            "timeInForce": "GTC",
+            "type": "MARKET",
+            "side": side,
+            "isIsolated": True,  # if isolated margin
+            "fills": [
+                {
+                    "price": str(price),
+                    "qty": str(quantity),
+                    "commission": str(0.001 * price),
+                    "commissionAsset": "USDT",
+                },
+            ],
+        }
