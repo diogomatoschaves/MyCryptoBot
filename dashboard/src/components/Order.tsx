a2d139ed6399a525a2f33085114425c06cad1186
Diogo Matos Chaves <diogo_chaves@hotmail.com>
2021-12-11 19:09:57 +0100

    Update Orders panel view

--- dashboard/src/components/Order.tsx
+++ dashboard/src/components/Order.tsx
@@ -1,5 +1,6 @@
 import {Order} from "../types";
-import {Table} from "semantic-ui-react";
+import {Label, Table} from "semantic-ui-react";
+import {BLUE, DARK_YELLOW, GREEN, PURPLE, RED, TEAL, YELLOW} from "../utils/constants";
 
 
 interface Props {
@@ -9,30 +10,39 @@ interface Props {
 
 function OrdersPanel(props: Props) {
 
-    const { order, index } = props
+  const { order, index } = props
 
-    const negative = order.side === 'SELL'
-    const positive = order.side === 'BUY'
+  const negative = order.side === 'SELL'
+  const positive = order.side === 'BUY'
 
-    const executedQty = Number(order.executedQty)
-    const origQty = Number(order.origQty)
-    const price = Number(order.price)
-    const total = price * executedQty
+  const color = negative ? RED : GREEN
 
-    const decimalPlaces = 3
+  const executedQty = Number(order.executedQty)
+  const origQty = Number(order.origQty)
+  const price = Number(order.price)
+  const total = price * executedQty
+
+  const decimalPlaces = 3
 
     // active={index % 2 == 0}
+  // positive={positive} negative={negative}
 
-    return (
-        <Table.Row positive={positive} negative={negative} key={index} >
+  return (
+        <Table.Row active={index % 2 == 0} key={index} >
             <Table.Cell style={styles.defaultCell}>{order.orderId}</Table.Cell>
-            <Table.Cell style={styles.defaultCell}>{order.transactTime}</Table.Cell>
+          <Table.Cell style={styles.defaultCell}>{order.transactTime.toUTCString()}</Table.Cell>
             <Table.Cell>{order.status}</Table.Cell>
-            <Table.Cell style={styles.defaultCell}>{order.symbol}</Table.Cell>
-            <Table.Cell>{order.side}</Table.Cell>
-            <Table.Cell style={styles.defaultCell}>{executedQty.toFixed(decimalPlaces)}</Table.Cell>
-            <Table.Cell style={styles.defaultCell}>{price.toFixed(decimalPlaces)}</Table.Cell>
-            <Table.Cell style={styles.defaultCell}>{total.toFixed(decimalPlaces)} {order.quote}</Table.Cell>
+            <Table.Cell style={{...styles.defaultCell, color: DARK_YELLOW}}>{order.symbol}</Table.Cell>
+            <Table.Cell style={{color, fontWeight: '600'}}>{order.side}</Table.Cell>
+            <Table.Cell style={{...styles.defaultCell, ...styles.quantityCell}}>
+              {executedQty.toFixed(decimalPlaces)}
+            </Table.Cell>
+            <Table.Cell style={{...styles.defaultCell, ...styles.quantityCell}}>
+              {price.toFixed(decimalPlaces)}
+            </Table.Cell>
+            <Table.Cell style={{...styles.defaultCell, ...styles.quantityCell}}>
+              {total.toFixed(decimalPlaces)} {order.quote}
+            </Table.Cell>
         </Table.Row>
     );
 }
@@ -44,5 +54,9 @@ const styles = {
     defaultCell: {
         color: 'rgb(70, 70, 70)',
         fontWeight: '500',
+    },
+    quantityCell: {
+      // color: TEAL,
+      fontWeight: '500',
     }
 }
