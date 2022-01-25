aa2056abdc62a916da75cc91730093318202d8ec
Diogo Matos Chaves <diogo_chaves@hotmail.com>
2022-01-25 06:41:53 -0400

    Add profit / loss to each pipeline

--- dashboard/src/types/index.ts
+++ dashboard/src/types/index.ts
@@ -51,6 +51,7 @@ export type Pipeline = {
     paperTrading: boolean
     openTime: Date | null
     numberTrades: number
+    profitLoss: number
 }
 
 
@@ -65,6 +66,7 @@ export type RawPipeline = {
     paperTrading: boolean
     openTime: string | null
     numberTrades: number
+    profitLoss: number
 }
 
 export type PipelineParams = {
