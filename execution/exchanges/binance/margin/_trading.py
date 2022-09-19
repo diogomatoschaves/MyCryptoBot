830bd2b5ce4d34875f8eb9f5b288f7e197bb5a9e
Diogo Matos Chaves <diogo_chaves@hotmail.com>
2022-09-19 17:56:17 +0200

    Refactor Binance trader to make common methods in an abstract class

--- execution/exchanges/binance/margin/_trading.py
+++ execution/exchanges/binance/margin/_trading.py
@@ -7,8 +7,7 @@ from datetime import datetime
 import django
 from binance.exceptions import BinanceAPIException
 
-from shared.exchanges import BinanceHandler
-from shared.trading import Trader
+from execution.exchanges.binance import BinanceTrader
 from execution.exchanges.binance.helpers import binance_error_handler
 from shared.utils.decorators.failed_connection import retry_failed_connection
 
@@ -18,7 +17,7 @@ django.setup()
 from database.model.models import Symbol, Orders, Position, Trade
 
 
-class BinanceMarginTrader(BinanceHandler, Trader):
+class BinanceMarginTrader(BinanceTrader):
 
     AUTO_REPAY = 'AUTO_REPAY'
 
@@ -27,8 +26,7 @@ class BinanceMarginTrader(BinanceHandler, Trader):
         margin_level=3,
         paper_trading=False
     ):
-        BinanceHandler.__init__(self, paper_trading)
-        Trader.__init__(self, 0)
+        BinanceTrader.__init__(self, paper_trading)
 
         self.paper_trading = paper_trading
         self.margin_level = margin_level
@@ -37,7 +35,6 @@ class BinanceMarginTrader(BinanceHandler, Trader):
         self.trading_fees = {}
         self.max_borrow_amount = {}
         self.symbols = {}
-        self.positions = {}
 
         self.open_orders = []
         self.filled_orders = []
@@ -127,20 +124,7 @@ class BinanceMarginTrader(BinanceHandler, Trader):
 
         self._set_position(symbol, 0, previous_position=1, **kwargs)
 
-        try:
-            perf = (self.current_balance - self.initial_balance) / self.initial_balance * 100
-        except ZeroDivisionError:
-            perf = 0
-
-        self.print_current_balance(date, header=header)
-        
-        header = header
-
-        logging.info(header + f"" + 100 * "-")
-        logging.info(header + f"{date} | +++ CLOSED FINAL POSITION +++")
-        logging.info(header + f"{date} | net performance (%) = {round(perf, 2)}")
-        logging.info(header + f"{date} | number of trades executed = {self.trades}")
-        logging.info(header + f"" + 100 * "-")
+        self.print_trading_results(header, date)
 
         return True
 
@@ -225,96 +209,6 @@ class BinanceMarginTrader(BinanceHandler, Trader):
         # TODO: Get this value from database?
         logging.debug(header + f"Setting initial position NEUTRAL.")
         self._set_position(symbol, 0)
-
-    def _set_position(self, symbol, position, **kwargs):
-
-        pipeline_id = kwargs.pop("pipeline_id", None)
-
-        self.positions[symbol] = position
-
-        previous_position = kwargs.pop("previous_position", 0)
-
-        new_trade = self._handle_trades(pipeline_id, symbol, previous_position, position)
-
-        if Position.objects.filter(pipeline_id=pipeline_id, symbol=symbol).exists():
-            if position == 0:
-                Position.objects.filter(
-                    pipeline_id=pipeline_id,
-                    symbol=symbol
-                ).update(
-                    position=position,
-                    open=False,
-                    close_time=datetime.now(tz=pytz.UTC)
-                )
-            else:
-                Position.objects.filter(
-                    pipeline_id=pipeline_id,
-                    symbol=symbol
-                ).update(
-                    position=position,
-                    open=True,
-                    close_time=None
-                )
-        elif new_trade:
-            Position.objects.create(
-                position=position,
-                symbol_id=symbol,
-                exchange_id=self.exchange,
-                pipeline_id=pipeline_id,
-                paper_trading=self.paper_trading,
-                buying_price=new_trade.open_price,
-                amount=new_trade.amount,
-            )
-
-    def _get_position(self, symbol):
-        return self.positions[symbol]
-
-    def _handle_trades(self, pipeline_id, symbol, previous_position, position):
-
-        number_orders = abs(previous_position - position)
-
-        orders = list(
-            Orders.objects.filter(
-                pipeline_id=pipeline_id,
-                symbol_id=symbol
-            ).order_by('-transact_time')[:number_orders]
-        )
-
-        if len(orders) == 0:
-            return None
-
-        if previous_position != 0:
-
-            closing_order = orders.pop(0)
-
-            last_trade = Trade.objects.filter(pipeline_id=pipeline_id, symbol_id=symbol).last()
-
-            if last_trade:
-                last_trade.close_price = closing_order.price
-                last_trade.close_time = datetime.now(tz=pytz.UTC)
-                last_trade.profit_loss = last_trade.side * (last_trade.close_price / last_trade.open_price - 1)
-                last_trade.save()
-
-        if position != 0:
-
-            new_order = orders.pop(0)
-
-            # amount = sum([order.executed_qty for order in orders])
-            # price = sum([order.price * order.executed_qty for order in orders]) / amount if amount > 0 else 0
-
-            new_trade = Trade.objects.create(
-                symbol_id=symbol,
-                open_price=new_order.price,
-                amount=new_order.executed_qty,
-                side=1 if new_order.side == "BUY" else -1,
-                exchange_id=self.exchange,
-                mock=new_order.mock,
-                pipeline_id=pipeline_id
-            )
-
-            return new_trade
-
-        return None
 
     @retry_failed_connection(num_times=3)
     def _get_trading_fees(self, symbol, header=''):
@@ -418,11 +312,6 @@ class BinanceMarginTrader(BinanceHandler, Trader):
 
         return kwargs
 
-    @staticmethod
-    def _get_average_order_price(order):
-        s = sum([float(fill["price"]) * float(fill["qty"]) for fill in order["fills"]])
-        return s / float(order["executedQty"])
-
     @retry_failed_connection(num_times=3)
     def repay_loans(self, symbol):
         for key, asset in self.symbols[symbol].items():
@@ -435,16 +324,3 @@ class BinanceMarginTrader(BinanceHandler, Trader):
                 )
             except BinanceAPIException:
                 pass
-
-    def report_trade(self, order, symbol, units, going, header=''):
-        logging.debug(order)
-
-        price = self._get_average_order_price(order)
-
-        date = datetime.fromtimestamp(order["transactTime"] / 1000).astimezone(pytz.utc)
-
-        logging.info(header + f"" + 100 * "-")
-        logging.info(header + f"{date} | {going}")
-        logging.info(header + f"units = {units} | price = {price}")
-        self.print_current_nav(date, price)
-        logging.info(header + f"" + 100 * "-")
