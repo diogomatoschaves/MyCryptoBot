efeb7ffb11376c8a00891ab58189101099b782a9
Diogo Matos Chaves <diogo_chaves@hotmail.com>
2022-10-29 13:36:19 +0200

    Refactor error handling in model app

---
+++ model/service/helpers/decorators/handle_app_errors.py
@@ -0,0 +1,26 @@
+import functools
+
+from flask import jsonify
+
+from shared.utils.exceptions import NoSuchPipeline
+
+
+def handle_app_errors(_func=None):
+    def retry(func):
+
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+
+            from model.service.helpers.responses import Responses
+
+            try:
+                return func(*args, **kwargs)
+            except NoSuchPipeline as e:
+                return jsonify(Responses.NO_SUCH_PIPELINE(e.message))
+
+        return wrapper
+
+    if _func is None:
+        return retry
+    else:
+        return retry(_func)
