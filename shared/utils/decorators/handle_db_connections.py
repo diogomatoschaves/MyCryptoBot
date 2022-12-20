f513da23b18ef411a7b557a0ad0e1dd2db03e5ac
Diogo Matos Chaves <diogo_chaves@hotmail.com>
2022-12-20 11:50:00 +0000

    Add decorator to handle database connection errors

---
+++ shared/utils/decorators/handle_db_connections.py
@@ -0,0 +1,41 @@
+import functools
+import logging
+
+from django import db
+from django.db import InterfaceError
+
+
+def process_retry(retries, num_times, e, exception):
+
+    retries += 1
+    if retries > num_times:
+        try:
+            raise e
+        except:
+            raise exception()
+
+    return retries
+
+
+def handle_db_connection_error(_func=None, *, num_times=2):
+    def retry(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            retries = 1
+            while True:
+                try:
+                    return func(*args, **kwargs)
+                except InterfaceError as e:
+                    logging.warning(e)
+
+                    for conn in db.connections.all():
+                        conn.close_if_unusable_or_obsolete()
+
+                    retries = process_retry(retries, num_times, e, InterfaceError)
+
+        return wrapper
+
+    if _func is None:
+        return retry
+    else:
+        return retry(_func)
