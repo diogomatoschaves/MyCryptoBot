794ad56402002a590b7a25bd4ad964735f26eca5
Diogo Matos Chaves <diogo_chaves@hotmail.com>
2023-01-25 10:36:00 +0100

    Prevent pipeline buttons to propagate and open pipeline detail view

--- dashboard/src/components/Pipeline.tsx
+++ dashboard/src/components/Pipeline.tsx
@@ -6,6 +6,7 @@ import styled from "styled-components";
 import PipelineButton from "./PipelineButton";
 import {timeFormatterDate} from "../utils/helpers";
 import {useState} from "react";
+import PipelineDeleteButton from "./PipelineDeleteButton";
 
 
 const StyledColumn = styled(Grid.Column)`
@@ -86,57 +87,13 @@ function PipelineItem(props: Props) {
                         </Grid.Column>
                     </Grid.Column>
                     <StyledColumn width={6} className="flex-row">
-                        <Modal
-                            onClose={() => setOpen(false)}
-                            onOpen={() => setOpen(true)}
+                        <PipelineDeleteButton
+                            pipeline={pipeline}
+                            deletePipeline={deletePipeline}
+                            stopPipeline={stopPipeline}
                             open={open}
-                            size='small'
-                            trigger={
-                                <div style={styles.buttonDiv} className='flex-column'>
-                                    <Button
-                                        icon
-                                        style={{width: '80%'}}
-                                    >
-                                        <span style={{marginRight: '10px', marginLeft: '-10px'}}>
-                                            <Icon name={'delete'}/>
-                                        </span>
-                                        Delete
-                                    </Button>
-                                </div>
-                            }
-                        >
-                            <Header icon>
-                                <Icon name='delete' />
-                                Delete trading bot
-                            </Header>
-                            <Modal.Content>
-                                <p>
-                                    Are you sure you want to delete this trading bot?
-                                </p>
-                            </Modal.Content>
-                            <Modal.Actions>
-                                <Button
-                                    color='red'
-                                    inverted
-                                    onClick={() => setOpen(false)}
-                                >
-                                    <Icon name='remove'/> No
-                                </Button>
-                                <Button
-                                    color='green'
-                                    inverted
-                                    onClick={async () => {
-                                        if (pipeline.active) {
-                                            await stopPipeline(pipeline.id)
-                                        }
-                                        deletePipeline(pipeline.id)
-                                        setOpen(false)
-                                    }}
-                                >
-                                    <Icon name='checkmark' /> Yes
-                                </Button>
-                            </Modal.Actions>
-                        </Modal>
+                            setOpen={setOpen}
+                        />
                     </StyledColumn>
                 </StyledRow>
                 <StyledRow>
@@ -165,13 +122,11 @@ function PipelineItem(props: Props) {
                         </Grid.Column>
                     </Grid.Column>
                     <StyledColumn width={6} className="flex-row">
-                        <div style={styles.buttonDiv} className='flex-column'>
-                            <PipelineButton
-                                pipeline={pipeline}
-                                startPipeline={startPipeline}
-                                stopPipeline={stopPipeline}
-                            />
-                        </div>
+                        <PipelineButton
+                            pipeline={pipeline}
+                            startPipeline={startPipeline}
+                            stopPipeline={stopPipeline}
+                        />
                     </StyledColumn>
                 </StyledRow>
                 {lastRow && (
@@ -224,8 +179,4 @@ const styles = {
         overflow: 'hidden',
         textOverflow: 'ellipsis'
     },
-    buttonDiv: {
-        width: '100%',
-        alignSelf: 'center'
-    }
 }
