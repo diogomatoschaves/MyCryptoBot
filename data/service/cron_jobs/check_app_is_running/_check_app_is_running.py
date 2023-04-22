389de6d8a95e831ae65e12398cd70f94e2cf7b20
Diogo Matos Chaves <diogo_chaves@hotmail.com>
2023-04-22 17:44:20 +0200

    Add new database model for keepiing track of portfolio value

--- data/service/cron_jobs/check_app_is_running/_check_app_is_running.py
+++ data/service/cron_jobs/check_app_is_running/_check_app_is_running.py
@@ -15,16 +15,16 @@ from data.service.blueprints.bots_api import stop_instance
 def check_app_is_running():
     """
     """
-    logging.info('Checking app status...')
+    logging.debug('Checking app status...')
 
     active_pipelines = Pipeline.objects.filter(active=True)
 
     for pipeline in active_pipelines:
 
-        logging.info(f'Checking pipeline {pipeline.id}...')
+        logging.debug(f'Checking pipeline {pipeline.id}...')
 
         if datetime.now(pytz.utc) - pipeline.last_entry > timedelta(minutes=10):
 
-            logging.info(f'{pipeline.id} found to be stuck. Sending stop request...')
+            logging.info(f'Pipeline {pipeline.id} found to be stuck. Sending stop request...')
 
             stop_instance(pipeline.id, '')
