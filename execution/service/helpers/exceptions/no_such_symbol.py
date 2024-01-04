525f9541258b4a8b982eb17f66f224010f2cc27a
Diogo Matos Chaves <diogo_chaves@hotmail.com>
2024-01-04 17:22:17 -0300

    Add tests for execution app exceptions

--- execution/service/helpers/exceptions/no_such_symbol.py
+++ execution/service/helpers/exceptions/no_such_symbol.py
@@ -2,8 +2,8 @@ class NoSuchSymbol(Exception):
     def __init__(self, *args):
         if not args:
             self.message = f"Symbol was not found."
-
-        self.message = f"Symbol {args[0]} was not found."
+        else:
+            self.message = f"Symbol {args[0]} was not found."
 
     def __str__(self):
         return f"{self.message}"
